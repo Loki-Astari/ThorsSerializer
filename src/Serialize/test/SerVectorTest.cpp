bf495a46ef43c74b76d676600df757980134f5a9
Loki Astari <Loki.Astari@gmail.com>
2015-02-16 12:03:39 -0800

    Added more tests
        Added de-serialize tests for vector and list

--- src/Serialize/test/SerVectorTest.cpp
+++ src/Serialize/test/SerVectorTest.cpp
@@ -29,4 +29,25 @@ TEST(SerVectorTest, VectorOfIntSerialize)
 
     EXPECT_EQ(result, R"([1,2,3,4,5,6,7,8,101,102,9,10])");
 }
+TEST(SerVectorTest, VectorOfIntDeSerialize)
+{
+    std::vector<int>    data;
+    std::stringstream   stream("[1,2,3,4,5,6,7,8,101,102,9,10]");
+
+    stream >> jsonImport(data);
+    auto loop = data.begin();
+
+    EXPECT_EQ(*loop++, 1);
+    EXPECT_EQ(*loop++, 2);
+    EXPECT_EQ(*loop++, 3);
+    EXPECT_EQ(*loop++, 4);
+    EXPECT_EQ(*loop++, 5);
+    EXPECT_EQ(*loop++, 6);
+    EXPECT_EQ(*loop++, 7);
+    EXPECT_EQ(*loop++, 8);
+    EXPECT_EQ(*loop++, 101);
+    EXPECT_EQ(*loop++, 102);
+    EXPECT_EQ(*loop++, 9);
+    EXPECT_EQ(*loop++, 10);
+}
 
