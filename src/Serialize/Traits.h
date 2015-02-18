5b0f2fca6fda98d459601dfb5eb03d7cdc16df73
Loki Astari <Loki.Astari@gmail.com>
2015-02-18 14:45:53 -0800

    Add a hashing function.
        This well be used by the Binary form to verify that we are
        extracting into an object with the same format as the type
        that generated the value.
    
        We need some validation as the binary format provides much fewer
        opportunities to check the raw input against what we expect (as it
        will be just data).

--- src/Serialize/Traits.h
+++ src/Serialize/Traits.h
@@ -170,6 +170,67 @@ template<> class Traits<bool>                   {public: static constexpr TraitT
 template<> class Traits<std::string>            {public: static constexpr TraitType type = TraitType::Value;};
 
 
+inline std::size_t thash(std::size_t& start, std::string const& input)
+{
+    static constexpr std::size_t    Mult = 37;
+    for(auto loop: input)
+    {
+        start = start * Mult + loop;
+    }
+    return start;
+}
+
+template<typename T>
+struct TraitsHash;
+
+
+template<typename T, typename M>
+inline std::size_t thash(std::size_t& start, std::pair<char const*, M T::*> const& member)
+{
+    TraitsHash<M>   typeHash;
+    return thash(typeHash(start), member.first);
+}
+template<typename T>
+struct TraitsHash
+{
+    template<typename Members, std::size_t... Seq>
+    std::size_t makeTraitsHashValue(std::size_t& start, Members const& member, std::index_sequence<Seq...> const&)
+    {
+        std::make_tuple({thash(start, std::get<Seq>(member))...});
+        return start;
+    }
+    template<typename... Members>
+    std::size_t makeTraitsHash(std::size_t& start, std::tuple<std::pair<char const*, Members>...>const& members)
+    {
+        makeTraitsHashValues(start, members, std::make_index_sequence<sizeof...(Members)>());
+        return start;
+    }
+    template<typename Action>
+    std::size_t makeTraitsHash(std::size_t& start, Action const& action)
+    {
+        return action.getHash(start);
+    }
+    std::size_t operator()(std::size_t& start)    {return makeTraitsHash(start, Traits<T>::getMembers());}
+};
+
+template<> struct TraitsHash<short>                 {std::size_t operator()(std::size_t& start){return thash(start, "S");}};
+template<> struct TraitsHash<int>                   {std::size_t operator()(std::size_t& start){return thash(start, "I");}};
+template<> struct TraitsHash<long>                  {std::size_t operator()(std::size_t& start){return thash(start, "L");}};
+template<> struct TraitsHash<long long>             {std::size_t operator()(std::size_t& start){return thash(start, "LL");}};
+
+template<> struct TraitsHash<unsigned short>        {std::size_t operator()(std::size_t& start){return thash(start, "US");}};
+template<> struct TraitsHash<unsigned int>          {std::size_t operator()(std::size_t& start){return thash(start, "UI");}};
+template<> struct TraitsHash<unsigned long>         {std::size_t operator()(std::size_t& start){return thash(start, "UL");}};
+template<> struct TraitsHash<unsigned long long>    {std::size_t operator()(std::size_t& start){return thash(start, "ULL");}};
+
+template<> struct TraitsHash<float>                 {std::size_t operator()(std::size_t& start){return thash(start, "F");}};
+template<> struct TraitsHash<double>                {std::size_t operator()(std::size_t& start){return thash(start, "D");}};
+template<> struct TraitsHash<long double>           {std::size_t operator()(std::size_t& start){return thash(start, "LD");}};
+
+template<> struct TraitsHash<bool>                  {std::size_t operator()(std::size_t& start){return thash(start, "B");}};
+template<> struct TraitsHash<std::string>           {std::size_t operator()(std::size_t& start){return thash(start, "S");}};
+
+
     }
 }
 
