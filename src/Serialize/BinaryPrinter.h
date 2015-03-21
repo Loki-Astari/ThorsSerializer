a6f21f9f8f88ce016f0f3edc17e0e8f677db62f2
Loki Astari <Loki.Astari@gmail.com>
2015-03-21 08:39:15 -0700

    Fix BinaryPrinter based on new BinaryRep code
        The binaryRep code was made consistent (but broken some interfaces)
        So I updated this code to conform to this new consistency.

--- src/Serialize/BinaryPrinter.h
+++ src/Serialize/BinaryPrinter.h
@@ -18,41 +18,22 @@ namespace ThorsAnvil
 template<typename T>
 class BinaryPrinter: public PrinterInterface
 {
-    void write(TBin::UInt128 value)
-    {
-        TBin::UInt128   networkValue    = TBin::thor_htobe128(value);
-        output.write(reinterpret_cast<char const*>(&networkValue), 16);
-    }
-    void write(TBin::UInt64 value)
-    {
-        TBin::UInt64    networkValue    = TBin::thor_htobe64(value);
-        output.write(reinterpret_cast<char const*>(&networkValue), 8);
-    }
-    void write(TBin::UInt32 value)
-    {
-        TBin::UInt32    networkValue    = htonl(value);
-        output.write(reinterpret_cast<char const*>(&networkValue), 4);
-    }
-    void write(TBin::UInt16 value)
-    {
-        TBin::UInt16    networkValue    = htons(value);
-        output.write(reinterpret_cast<char const*>(&networkValue), 2);
-    }
-    void write(unsigned char value)
-    {
-        output.write(reinterpret_cast<char const*>(&value), 1);
-    }
+    void write(TBin::BinForm128 value)  {output.write(reinterpret_cast<char const*>(&value), 16);}
+    void write(TBin::BinForm64 value)   {output.write(reinterpret_cast<char const*>(&value), 8); }
+    void write(TBin::BinForm32 value)   {output.write(reinterpret_cast<char const*>(&value), 4); }
+    void write(TBin::BinForm16 value)   {output.write(reinterpret_cast<char const*>(&value), 2); }
+    void write(unsigned char value)     {output.write(reinterpret_cast<char const*>(&value), 1); }
     void write(std::string const& value)
     {
-        TBin::UInt32    size            = value.size();
-        write(size);
+        TBin::BinForm32    size            = value.size();
+        write(TBin::host2Net(size));
         output.write(value.c_str(), size);
     }
     public:
         BinaryPrinter(std::ostream& output, OutputType characteristics = OutputType::Default)
             : PrinterInterface(output, characteristics)
         {}
-        virtual void openDoc()                              override    {write(static_cast<TBin::UInt32>(thash<T>()));}
+        virtual void openDoc()                              override    {write(TBin::host2Net(static_cast<TBin::BinForm32>(thash<T>())));}
         virtual void closeDoc()                             override {}
 
         virtual void openMap()                              override {}
@@ -62,15 +43,15 @@ class BinaryPrinter: public PrinterInterface
 
         virtual void addKey(std::string const& key)         override    {write(key);}
 
-        virtual void addValue(short int value)              override    {write(static_cast<TBin::UInt16>(value));}
-        virtual void addValue(int value)                    override    {write(static_cast<TBin::UInt32>(value));}
-        virtual void addValue(long int value)               override    {write(static_cast<TBin::UInt64>(value));}
-        virtual void addValue(long long int value)          override    {write(static_cast<TBin::UInt128>(value));}
+        virtual void addValue(short int value)              override    {write(TBin::host2Net(static_cast<TBin::BinForm16>(value)));}
+        virtual void addValue(int value)                    override    {write(TBin::host2Net(static_cast<TBin::BinForm32>(value)));}
+        virtual void addValue(long int value)               override    {write(TBin::host2Net(static_cast<TBin::BinForm64>(value)));}
+        virtual void addValue(long long int value)          override    {write(TBin::host2Net(static_cast<TBin::BinForm128>(value)));}
 
-        virtual void addValue(unsigned short int value)     override    {write(static_cast<TBin::UInt16>(value));}
-        virtual void addValue(unsigned int value)           override    {write(static_cast<TBin::UInt32>(value));}
-        virtual void addValue(unsigned long int value)      override    {write(static_cast<TBin::UInt64>(value));}
-        virtual void addValue(unsigned long long int value) override    {write(static_cast<TBin::UInt128>(value));}
+        virtual void addValue(unsigned short int value)     override    {write(TBin::host2Net(static_cast<TBin::BinForm16>(value)));}
+        virtual void addValue(unsigned int value)           override    {write(TBin::host2Net(static_cast<TBin::BinForm32>(value)));}
+        virtual void addValue(unsigned long int value)      override    {write(TBin::host2Net(static_cast<TBin::BinForm64>(value)));}
+        virtual void addValue(unsigned long long int value) override    {write(TBin::host2Net(static_cast<TBin::BinForm128>(value)));}
 
         virtual void addValue(float value)                  override    {write(TBin::convertIEEE(value));}
         virtual void addValue(double value)                 override    {write(TBin::convertIEEE(value));}
