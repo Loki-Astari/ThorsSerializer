47576ebde47bd56d82629488f13131679386ddfb
Loki Astari <Loki.Astari@gmail.com>
2023-07-23 10:40:24 -0700

    Fix Header only failure

--- src/Serialize/MongoUtility.cpp
+++ src/Serialize/MongoUtility.cpp
@@ -13,20 +13,25 @@ using namespace ThorsAnvil::Serialize::MongoUtility;
 
 int ObjectID::classCounter = 32769;
 
+HEADER_ONLY_INCLUDE
 ObjectID::ObjectID(std::int32_t timestamp, std::int64_t random, std::int32_t counter)
     : timestamp(timestamp)
     , random(random)
     , counter(counter)
 {}
 
+HEADER_ONLY_INCLUDE
 UTCDateTime::UTCDateTime(std::int64_t datetime)
     : datetime(datetime)
 {}
+
+HEADER_ONLY_INCLUDE
 BsonTimeStamp::BsonTimeStamp(std::time_t timestamp, int inc)
     : increment(inc)
     , timestamp(timestamp)
 {}
 
+HEADER_ONLY_INCLUDE
 UTCDateTime BsonTimeStamp::asDateTime()
 {
     return UTCDateTime(timestamp * 1000);
