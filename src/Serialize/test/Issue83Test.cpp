77215e983e3dfc73bbc5a9ef4907b8efcad20ab7
Loki Astari <Loki.Astari@gmail.com>
2023-12-14 09:55:18 -0800

    Test example of Issue83

---
+++ src/Serialize/test/Issue83Test.cpp
@@ -0,0 +1,36 @@
+#include "SerializeConfig.h"
+#include "gtest/gtest.h"
+#include "test/SerializeTest.h"
+#include "Serialize.h"
+#include "Traits.h"
+#include "JsonThor.h"
+#include "BsonThor.h"
+#include "SerUtil.h"
+#include <memory>
+#include <iostream>
+
+struct Example
+{
+    int x{0};
+};
+struct Adeek
+{
+    std::shared_ptr<Example> member;
+};
+
+ThorsAnvil_MakeTrait(Example, x);
+ThorsAnvil_MakeTrait(Adeek, member);
+
+
+
+TEST(Issue83Test, test1)
+{
+    using ThorsAnvil::Serialize::jsonExporter;
+
+    Adeek obj;
+    std::stringstream   outputJSONStream;
+
+    outputJSONStream << jsonExporter(obj);
+};
+
+
