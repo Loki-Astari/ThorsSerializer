a9667a99d217431ce5fc4d42c7ed4a2e894834f5
Loki Astari <Loki.Astari@gmail.com>
2024-07-29 15:41:58 -0700

    Add support for template template parameters.
        template<template<typename> T>
        struct X
        {
            // Here T is a template that can take a single parameter.
        };

---
+++ src/Serialize/test/TemplateTemplateTest.cpp
@@ -0,0 +1,63 @@
+#include "gtest/gtest.h"
+#include "Traits.h"
+#include "JsonThor.h"
+#include "ThorsSerializerUtil.h"
+
+
+template<typename T>
+struct Time
+{
+    T   time;
+};
+
+struct SetDate
+{
+    std::string     type = "date";
+};
+struct SetTimeStamp
+{
+    std::string     type = "timestamp";
+};
+
+template<typename T>
+concept IsDateType = requires {T::type;};
+
+template<template<typename> typename T, typename L>
+struct CurrentDate
+{
+    T<L>    currentDate;
+};
+
+
+ThorsAnvil_Template_MakeTrait(1, Time, time);
+ThorsAnvil_MakeTrait(SetDate, type);
+ThorsAnvil_MakeTrait(SetTimeStamp, type);
+// Notice the TT here.
+//  This indicates that the first template parameter is a template.
+//  That can take a single parameter.
+ThorsAnvil_TTemplate_MakeTrait(2, CurrentDate, currentDate);
+
+TEST(TemplateTemplateTest, SerializeATemplateTemplateDate)
+{
+    using ThorsAnvil::Serialize::jsonExporter;
+    using ThorsAnvil::Serialize::PrinterInterface;
+
+    CurrentDate<Time, SetDate>  time;
+    std::stringstream   stream;
+    stream << jsonExporter(time, PrinterInterface::PrinterConfig{PrinterInterface::OutputType::Stream});
+
+    EXPECT_EQ(R"({"currentDate":{"time":{"type":"date"}}})", stream.str());
+}
+TEST(TemplateTemplateTest, SerializeATemplateTemplateTimeStamp)
+{
+    using ThorsAnvil::Serialize::jsonExporter;
+    using  ThorsAnvil::Serialize::PrinterInterface;
+
+    CurrentDate<Time, SetTimeStamp>  time;
+    std::stringstream   stream;
+    stream << jsonExporter(time, PrinterInterface::PrinterConfig{PrinterInterface::OutputType::Stream});
+
+    EXPECT_EQ(R"({"currentDate":{"time":{"type":"timestamp"}}})", stream.str());
+}
+
+
