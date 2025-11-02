7befffc36eb8ef129d677285d3c89f4d02247de5
Loki Astari <Loki.Astari@gmail.com>
2025-11-02 09:57:55 -0800

    Add macros to log JSON data

---
+++ src/Serialize/Logging.h
@@ -0,0 +1,24 @@
+#ifndef THORSANVIL_SERIALIZE_LOGGING_H
+#define THORSANVIL_SERIALIZE_LOGGING_H
+
+#include "JsonThor.h"
+
+#define THORS_LOG_DATA_SERIAL                     ThorsAnvil::Serialize::PrinterConfig{ThorsAnvil::Serialize::OutputType::Stream}
+#define THORS_LOG_DATA(D)                         "|Data|", ThorsAnvil::Serialize::jsonExporter(D, LOG_DATA_SERIAL)
+
+#define ThorsLogAndThrowFatalWithData(E, D, ...)   ThorsLogAndThrowFatal(E,   __VA_ARGS__, THORS_LOG_DATA(D))
+#define ThorsLogAndThrowErrorWithData(E, D, ...)   ThorsLogAndThrowError(E,   __VA_ARGS__, THORS_LOG_DATA(D))
+#define ThorsLogAndThrowWarningWithData(E, D, ...) ThorsLogAndThrowWarning(E, __VA_ARGS__, THORS_LOG_DATA(D))
+#define ThorsLogAndThrowInfoWithData(E, D, ...)    ThorsLogAndThrowInfo(E,    __VA_ARGS__, THORS_LOG_DATA(D))
+#define ThorsLogAndThrowDebugWithData(E, D, ...)   ThorsLogAndThrowDebug(E,   __VA_ARGS__, THORS_LOG_DATA(D))
+#define ThorsLogAndThrowTraceWithData(E, D, ...)   ThorsLogAndThrowTrace(E,   __VA_ARGS__, THORS_LOG_DATA(D))
+#define ThorsLogAndThrowAllWithData(E, D, ...)     ThorsLogAndThrowAll(E,     __VA_ARGS__, THORS_LOG_DATA(D))
+#define ThorsLogFatalWithData(D, ...)              ThorsLogFatal(  __VA_ARGS__, THORS_LOG_DATA(D))
+#define ThorsLogErrorWithData(D, ...)              ThorsLogError(  __VA_ARGS__, THORS_LOG_DATA(D))
+#define ThorsLogWarningWithData(D, ...)            ThorsLogWarning(__VA_ARGS__, THORS_LOG_DATA(D))
+#define ThorsLogInfoWithData(D, ...)               ThorsLogInfo(   __VA_ARGS__, THORS_LOG_DATA(D))
+#define ThorsLogDebugWithData(D, ...)              ThorsLogDebug(  __VA_ARGS__, THORS_LOG_DATA(D))
+#define ThorsLogTraceWithData(D, ...)              ThorsLogTrace(  __VA_ARGS__, THORS_LOG_DATA(D))
+#define ThorsLogAllWithData(D, ...)                ThorsLogAll(    __VA_ARGS__, THORS_LOG_DATA(D))
+
+#endif
